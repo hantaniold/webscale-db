b9b564ff256fa3640967a1844b5fd6b7051e7a94
Charles Cary <cclego@gmail.com>
2012-05-07 17:29:56 -0500

    added EQ command to dbm and some more declarations in dbm.h

--- chidb/src/libchidb/dbm.c
+++ chidb/src/libchidb/dbm.c
@@ -5,6 +5,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 /*
 *  This file implements the dbm.
@@ -28,7 +29,7 @@ int reset_dbm(dbm *input_dbm) {
 	return CHIDB_OK;
 }
 
-//TODO: ERROR HANDLING
+//TODO: BETTER ERROR HANDLING
 int tick_dbm(dbm *input_dbm, chidb_stmt stmt) {
 	switch (stmt.instruction) {
 		case DBM_OPENWRITE:
@@ -36,6 +37,7 @@ int tick_dbm(dbm *input_dbm, chidb_stmt stmt) {
 			uint32_t page_num = (input_dbm->registers[stmt.P2]).data.int_val;
 			input_dbm->cursors[stmt.P1].node = (BTreeNode *)calloc(1, sizeof(BTreeNode));
 			if (chidb_Btree_getNodeByPage(input_dbm->db->bt, page_num, &(input_dbm->cursors[stmt.P1].node)) == CHIDB_OK) {
+				input_dbm->program_counter += 1;
 				return DBM_OK;
 			} else {
 				return DBM_OPENRW_ERROR;
@@ -46,6 +48,7 @@ int tick_dbm(dbm *input_dbm, chidb_stmt stmt) {
 				free(input_dbm->cursors[stmt.P1].curr_cell);
 				free(input_dbm->cursors[stmt.P1].prev_cell);
 				free(input_dbm->cursors[stmt.P1].next_cell);
+				input_dbm->program_counter += 1;
 				return DBM_OK;
 			} else {
 				return DBM_GENERAL_ERROR;
@@ -80,6 +83,41 @@ int tick_dbm(dbm *input_dbm, chidb_stmt stmt) {
 		case DBM_INSERT:
 			break;
 		case DBM_EQ:
+			if (input_dbm->registers[stmt.P1].type == input_dbm->registers[stmt.P3].type) {
+				switch (input_dbm->registers[stmt.P1].type) {
+					case INTEGER:
+						if (input_dbm->registers[stmt.P1].data.int_val == input_dbm->registers[stmt.P3].data.int_val) {
+							input_dbm->program_counter = stmt.P2;
+						} else {
+							input_dbm->program_counter += 1;	
+						}
+					break;
+					case STRING:
+						if (strcmp(input_dbm->registers[stmt.P1].data.str_val, input_dbm->registers[stmt.P3].data.str_val) == 0){
+							input_dbm->program_counter = stmt.P2;
+						} else {
+							input_dbm->program_counter += 1;	
+						}
+					break;
+					case BINARY:
+						if (input_dbm->registers[stmt.P1].data_len == input_dbm->registers[stmt.P3].data_len) {
+							if (memcmp(input_dbm->registers[stmt.P1].data.bin_val, input_dbm->registers[stmt.P3].data.bin_val, input_dbm->registers[stmt.P1].data_len) == 0) {
+							input_dbm->program_counter = stmt.P2;
+							} else {
+								input_dbm->program_counter += 1;	
+							}
+						} else {
+							return DBM_DATA_REGISTER_LENGTH_MISMATCH;
+						} 
+					break;
+					case NL:
+						input_dbm->program_counter = stmt.P2;
+					break;
+				}
+				return DBM_OK;
+			} else {
+					return DBM_REGISTER_TYPE_MISMATCH;
+			}
 			break;
 		case DBM_NE:
 			break;
